62a9853230e622b2276a71502037617dded7c7d4
Youssef Ibrahim <yi9209690@gmail.com>
2025-09-10 14:58:23 +0300

    add

---
+++ F. Reversing.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int A[1000]; 
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+
+    for (int i = N - 1; i >= 0; i--) {
+        cout << A[i];
+        if (i > 0) cout << " "; 
+    }
+    cout << endl;
+
+    return 0;
+}
